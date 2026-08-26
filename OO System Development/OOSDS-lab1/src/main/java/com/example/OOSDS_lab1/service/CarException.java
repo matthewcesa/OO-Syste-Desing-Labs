11cecbbf1bb1df3e11394a535865e15edd299396
Matthew <matthewcesa405@gmail.com>
2026-08-26 17:05:57 +0200

    exception handling begin

---
+++ OO System Development/OOSDS-lab1/src/main/java/com/example/OOSDS_lab1/service/CarException.java
@@ -0,0 +1,10 @@
+package com.example.OOSDS_lab1.service;
+
+public class CarException extends Exception{
+
+    public CarException(String message){
+        super(message);
+    }
+    public CarException()
+    {super();}
+}
